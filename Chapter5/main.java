f69b8c7968f5c457ae78f47004a5079afa26d927
Kosan3 <ichiro.neko.0421@gmail.com>
2020-10-19 19:00:40 +0900

    222

--- Chapter5/main.java
+++ Chapter5/main.java
@@ -1,15 +1,13 @@
 public class Main {
-  public static void incArray(int[] array) {
-    for (int i = 0; i < array.length; i++) {
-      array[i]++;
-    }
+  public static double calcTriangleArea(double bottom, double height) {
+    return bottom * height / 2;
+  }
+  public static double calcCircleArea(double radius) {
+    return radius * radius * 3.14;
   }
   public static void main(String[] args) {
-    int[] array = {1, 2, 3};
-    incArray(array);
-    for (int i : array) {
-      System.out.println(i);
-    }
-    System.out.println(args[0]+ args[1]);
+    double triangleArea = calcTriangleArea(10.0, 5.0);
+    double circleArea = calcCircleArea(5.0);
+    System.out.println("三角形の面積は" + triangleArea + "cm\n円の面積は" + circleArea + "cm");
   }
 }
