d517763a7ba5c49886901bc9ecdf40d9de22e599
Kosan3 <ichiro.neko.0421@gmail.com>
2020-10-19 17:49:23 +0900

    207

---
+++ Chapter5/main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("あああ");
+    hello("やすし");
+    System.out.println("いいい");
+  }
+  public static void hello(String name) {
+    System.out.println("こんにちは" + name + "さん");
+  }
+}
