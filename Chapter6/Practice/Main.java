e10ea8fe2ce08f1b538a30b8bf3f5115cec68637
Kosan3 <ichiro.neko.0421@gmail.com>
2020-10-20 09:43:33 +0900

    267

---
+++ Chapter6/Practice/Main.java
@@ -0,0 +1,11 @@
+import comment.Zenhan;
+import comment.Kouhan;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    Zenhan.doWarusa();
+    Zenhan.doTogame();
+    Kouhan.callDeae();
+    Kouhan.showMondokoro();
+  }
+}
