6ba0ac7dbc53582c9329c58745518b2bc42e037c
Kosan3 <ichiro.neko.0421@gmail.com>
2020-10-21 18:06:35 +0900

    c8

--- Chapter8/Practice/Cleric.java
+++ Chapter8/Practice/Cleric.java
@@ -1,25 +1,26 @@
 import java.util.Random;
 
 public class Cleric {
-  String name;
   int hp = 50;
   final int MAX_HP = 50;
   int mp = 10;
   final int MAX_MP = 10;
 
-  public void selfAid() {
+  public void selfAId() {
+    System.out.println("セルフエイドを発動");
+    System.out.println("HPを全回復");
     this.mp -= 5;
-    System.out.println("MPを5消費し" + this.mp + "MP");
     this.hp = this.MAX_HP;
-    System.out.println(this.name + "のHPは全回復");
+    System.out.println("mpは" + this.mp + "に減少");
   }
 
-  public void pray(int sec) {
-    System.out.println(this.name + "は" + sec + "秒間祈った...");
-    Random random = new Random();
-    int recovery = sec + random.nextInt(3);
-    int real_recovery = Math.min(this.MAX_MP - this.mp, recovery);
-    this.mp += real_recovery;
-    System.out.println(this.name + "は" + real_recovery + "回復し" + this.mp + "MP");
+  public int pray(int sec) {
+    System.out.println("祈った");
+    int recovery = new Random().nextInt(3) + sec;
+    int realRecovery = Math.min(recovery, this.MAX_MP - this.mp);
+    this.mp += realRecovery;
+    System.out.println("mpは" + this.mp + "に回復");
+    return realRecovery;
   }
+
 }
--- Chapter8/Practice/Main.java
+++ Chapter8/Practice/Main.java
@@ -1,12 +1,9 @@
 public class Main {
   public static void main(String[] args) {
     Cleric c = new Cleric();
-    c.name = "千波さん";
-
-    c.selfAid();
-    c.pray(3);
-    c.pray(3);
-    c.pray(3);
-    
+    c.selfAId();
+    c.pray(1);
+    c.pray(1);
+    c.pray(1);
   }
 }
