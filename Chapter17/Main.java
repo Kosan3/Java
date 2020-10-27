9ca3d53a3c18c47eb1b48eaf9b4f3e7dc89ca536
Kosan3 <ichiro.neko.0421@gmail.com>
2020-10-27 14:54:33 +0900

    692

---
+++ Chapter17/Main.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException{
+    System.out.println("プログラムを開始します");
+    throw new IOException("例外をだしました。");
+  }
+}
